9325602f3cbda951722ce1c29993b416eaed0fff
rick-rodriguez16 <rickrod6502@gmail.com>
2025-03-28 23:52:17 -0400

    Updated jamgit-init directory creation

--- bin/jamgit-init.js
+++ bin/jamgit-init.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
-
 console.log("Hello from jamgit-init!")
+import { makeDirectory, makeMultiLevelDirectories } from "../lib/file-system/dir-create";
+
+const rootDir = './.jamgit';
+const branchesDir = `${rootDir}/branches`;
+const hooksDir = `${rootDir}/hooks`;
+const infoDir = `${rootDir}/info`;
+const objectsInfoDir = `${rootDir}/objects/info`;
+const objectsPackDir = `${rootDir}/objects/pack`;
+const refsHeadsDir = `${rootDir}/refs/heads`;
+const refsTagsDir = `${rootDir}/refs/tags`;
+
+
+try {
+    makeDirectory(rootDir);
+    makeMultiLevelDirectories(branchesDir);
+    makeMultiLevelDirectories(hooksDir);
+    makeMultiLevelDirectories(infoDir);
+    makeMultiLevelDirectories(objectsInfoDir);
+    makeMultiLevelDirectories(objectsPackDir);
+    makeMultiLevelDirectories(refsHeadsDir);
+    makeMultiLevelDirectories(refsTagsDir);
+} catch (error) {
+    console.error("Error: ", error)
+}
+
+
+
+
